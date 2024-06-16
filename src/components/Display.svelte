c0962d0610ad92daae2afa828dd21a0b601d7d40
hoy git chew <iammusicchu@gmail.com>
2024-06-16 16:11:45 +0800

    1st git commit

---
+++ src/components/Display.svelte
@@ -0,0 +1,7 @@
+<script>
+  import {count} from '../store/stores'
+
+
+</script>
+<hr />
+<h2> Current Count is {$count}</h2>
---
+++ src/components/Timer.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {time} from  '../stores.jsx' 
+
+const dateFormatter = new Intl.DateTimeFormat('en', {
+  hour12: true,
+  hour: 'numeric',
+  minute: '2-digit',
+  second: '2-digit',
+})
+</script>
+
+<h2>Trhe time is {dateFormatter.format($time)}</h2>
+
+<h2>from Timer.svelte</h2>
