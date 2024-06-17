fc8c6d19a1a0a9e8520204f96d9eb794043de45e
hoy git chew <iammusicchu@gmail.com>
2024-06-17 11:48:26 +0800

    4th another clone window

--- gitTest1.js
+++ gitTest1.js
@@ -1,3 +1,4 @@
 const gitTest = "gitTest1 yes from windows my";
 const gitTest2 = "from windows 2nd change";
 const gitTest3 = "3rd change ";
+const gitTest4 = "4th change from another clone in windows 11:44am 17, jun 2024"
