b625d56f1adb55895aab0ab89813205936f1a7dc
hoy git chew <iammusicchu@gmail.com>
2024-06-17 13:02:51 +0800

    8th second clone window

--- gitTest1.js
+++ gitTest1.js
@@ -4,3 +4,4 @@ const gitTest3 = "3rd change ";
 const gitTest4 = "4th change from another clone in windows 11:44am 17, jun 2024"
 const  gitTest5 = "5th change from vs code window"
 const gitTest7 = "7th change from code window"
+const gitTest8  = "8th change from 2nd clone window."
