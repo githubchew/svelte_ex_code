3d3fc9e3068c52dcd7e3ccac40274cd79fae35e4
Hubchew <iammusicchu@gmail.com>
2024-06-18 10:38:28 +0800

    delete everythin

--- src/components/Display.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import {count} from '../store/stores'
-
-
-</script>
-<hr />
-<h2> Current Count is {$count}</h2>
--- src/components/Reset.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import {count} from '../store/stores'
-    function reset() {
-
-      count.set(0)
-
-    }
-</script>
-<button on:click={reset}>Reset</button>
