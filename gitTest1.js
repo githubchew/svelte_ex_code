42ba96429a1c531f26eade776d30a7592ea12f4f
Hubchew <iammusicchu@gmail.com>
2024-06-18 12:48:49 +0800

    1st jsmilga calcualtro

--- gitTest1.js
+++
@@ -1,7 +0,0 @@
-const gitTest = "gitTest1 yes from windows my";
-const gitTest2 = "from windows 2nd change";
-const gitTest3 = "3rd change ";
-const gitTest4 = "4th change from another clone in windows 11:44am 17, jun 2024"
-const  gitTest5 = "5th change from vs code window"
-const gitTest7 = "7th change from code window"
-const gitTest8  = "8th change from 2nd clone window."
---
+++ src/Navbar.svelte
@@ -0,0 +1,10 @@
+<nav class="nav">
+    <div class="nav-center">
+  
+      <h1 class="nav-title">budget calculator</h1>
+      
+    </div>
+    <button type="button" class="nav-btn">
+      + add item
+    </button>
+  </nav>
