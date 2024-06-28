153fd2d2cb8991020c0c944682685d1c64e2bd89
lmde-git-hoychew <iammusiccchu@gmail.com>
2024-06-28 18:54:14 +0800

    magic 29m

--- Magic counter app svelte/src/App.svelte
+++ Magic counter app svelte/src/App.svelte
@@ -1,12 +1,24 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Player from './Player.svelte'
+  let redScore = 20
+  let blueScore = 233
+  let blueWon = true
+  let redWon = true
+  let gameOver  = true;
 </script>
-
+//------------------------------------------------------@29:00
 <main>
+  <a href="https://www.youtube.com/watch?v=M_teofI6vKY&list=PLT9tklUafCrKINVnRGFiqMU-b0zFyR6TG">yt links</a>
   <h1>Magic the Gathering Counter App</h1>
-  <div id="controls-container"></div>
+  <div id="controls-container">
+    //BLUE
+    <Player {gameOver}
+    fontColor="00A" won={blueWon} winningText="blue wins"
+    score={blueScore} />
+    //RED 
+    <Player  {gameOver}
+    fontColor="A00" won={redWon} winningText="red w" score={redScore} />
+  </div>
   <button>Start Game</button>
 
 </main>
@@ -29,7 +41,7 @@
     margin-top: 20px;
     border: solid salmon 2px;
     border-color: saddlebrown;
-    color:rgb(65,45,56);
+    color:rgb(239, 161, 204);
     font-size: 20px;
     border-radius: 3px;
   }
---
+++ Magic counter app svelte/src/Player.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let score 
+    export let winningText;
+    export let won
+    export let fontColor
+    export let gameOver
+</script>
+<div class="player" style="color:{fontColor}">
+    <h2>score: {score}</h2>
+    <button class="plus" disabled={gameOver}>+</button>
+    <button class="minus" disabled={gameOver}>-</button>
+     {#if won}
+    <h2>{winningText}</h2>
+    {/if}
+</div>
+
+<style>
+    button[disabled] {
+        background-color:gray;
+        pointer-events:none;
+    }
+    .player {
+        flex-grow: 1;
+
+    }
+    .plus {
+        background-color: seagreen;
+
+    }
+    .minus {
+        background-color: brown;
+    }
+    button {
+        font-size: 20px;
+        border-radius: 3px;
+        width:40px;
+        color:white;
+        font-family: monospace;
+        font-weight: bold;
+    }
+</style>
